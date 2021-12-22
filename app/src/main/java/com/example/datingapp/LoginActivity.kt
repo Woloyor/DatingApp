9d1495e21dddb144b7001fca84e8990b50f9dd41
Yura Lysyshak <yuralysyshak@gmail.com>
2021-12-23 01:45:10 +0200

    NEED LOGIN BACK-END

--- app/src/main/java/com/example/datingapp/LoginActivity.kt
+++ app/src/main/java/com/example/datingapp/LoginActivity.kt
@@ -1,10 +1,13 @@
 package com.example.datingapp
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
+import android.util.Log
+import android.util.Patterns
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -20,36 +23,56 @@ class LoginActivity : AppCompatActivity() {
         auth = Firebase.auth
 
 
-
-        val start = findViewById<TextView>(R.id.foget)
-        start.setOnClickListener {
-            val intent = Intent(this, ForgotPassowrdActivity::class.java)
+        val login = findViewById<ImageButton>(R.id.btncontinue)
+        login.setOnClickListener {
+            val intent = Intent(this, SwapActivity::class.java)
             startActivity(intent)
+        }
+
+
+        val start = findViewById<Button>(R.id.foget)
+
+
+        start.setBackgroundColor(0x00000000);
+        start.setOnClickListener {
+
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Forgot Password")
+            val view = layoutInflater.inflate(R.layout.dialog_forgot_password, null)
+            val username = view.findViewById<EditText>(R.id.et_username)
+            builder.setView(view)
+            builder.setPositiveButton("Reset", DialogInterface.OnClickListener { _, _ ->
+                forgotPassword(username)
+            })
+            builder.setNegativeButton("Close", DialogInterface.OnClickListener { _, _ -> })
+            builder.show()
+
+
         }
         val backbtn = findViewById<ImageView>(R.id.backbtn);
 
-        backbtn.setOnClickListener{
+        backbtn.setOnClickListener {
             val intent = Intent(this, LoginAndSingInActivity::class.java)
             startActivity(intent)
         }
 
+    }
+    private fun forgotPassword(username: EditText) {
+        if (username.text.toString().isEmpty()){
+            return
         }
 
-    /*public override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        if(currentUser != null){
-            reload();
+        if (!Patterns.EMAIL_ADDRESS.matcher(username.text.toString()).matches())
+        {
+            return
         }
+        auth.sendPasswordResetEmail(username.text.toString())
+            .addOnCompleteListener{ task  ->
+                if(task.isSuccessful)(
+                        Toast.makeText(this, "Email sent.", Toast.LENGTH_SHORT).show()
+                        )
+            }
     }
-
-    private fun reload() {
-
-        TODO("Not yet implemented")
-    }
-
-
-     */
-
 }
+
+
